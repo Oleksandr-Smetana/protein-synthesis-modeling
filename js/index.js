566e296cf555ece43730acfe9b19dc2ebb9ff91d
Oleksandr-Smetana <anatemso@gmail.com>
2021-09-01 20:23:18 +0300

    добавляет разметку, стили
    + объект с таблицей генетического кода

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import standardCode from "./standard-code";
+
+const refs = {
+  inputElt: document.querySelector(".input"),
+  buttonElt: document.querySelector(".start-modeling-button"),
+  modelingResultElt: document.querySelector(".protein-sequence"),
+};
